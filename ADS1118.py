6672a632431043188faeae70ffe6d38763631f63
jonathanimb <jon.brauer@gmail.com>
2017-07-28 12:53:40 -0700

    Update ADS1118.py

--- ADS1118.py
+++ ADS1118.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 
+"""
+A class to read data from the ADS1118 16-bit ADC chip.
+Single shot mode only (for now)
+Bit bang only (for now)
+"""
+
 import time
-import math
+
 import RPi.GPIO as GPIO
 
 class ConnectionError(Exception):
@@ -111,7 +117,7 @@ def verify(command, config):
     if config[1:15] != command[1:15]:
         raise ConnectionError('sum of bits: {}'.format(sum(config)))
 
-def pause(command, start_time=None):
+def pause(command):
     '''wait for the amount of time the command takes to execute'''
     time.sleep(sleep_times[list_to_int(command[8:11])])
 
@@ -168,8 +174,7 @@ class ADS1118(object):
         # another method to accomplish the same
         # this method is slower than above, and slower than the pause function
         # in addition it has the tendancy to corrupt data
-        # I left this in here to prove I've tested it because otherwise I'd get
-        # a ton of PRs from smart people. smart people: test it for yourself
+        # I left this in here to prove I've tested it 
         #~ if GPIO.input(self.DOUT):
             #~ GPIO.wait_for_edge(self.DOUT, GPIO.FALLING, timeout=40)
 
@@ -225,7 +230,6 @@ class ADS1118(object):
                 pause(command)
             else:
                 self.wait()
-
         responses.append(self._read(INT_TEMP)) # dummy command to get the last data value
         configs, datas = zip(*responses)
         results = []
@@ -233,76 +237,4 @@ class ADS1118(object):
             results.append(interpret(config, data))
             verify(command, config)
         return results
-
-def TC_linearize(coldJunctionTemperature, thermocoupleVoltage):
-    '''
-    Use the reference temperature and voltage from a type K thermocouple
-    to calculate actual temperature.
-
-    :coldJunctionTemperature: temperature of the TC junction in degrees C
-    :thermocoupleVoltage: reading from the thermocouple in volts
-
-    :returns: the temperature of the themocouple in degrees C
-    '''
-    thermocoupleVoltage *= 1000.
-    coldJunctionVoltage = (-0.176004136860E-01 +
-        0.389212049750E-01  * coldJunctionTemperature +
-        0.185587700320E-04  * math.pow(coldJunctionTemperature, 2.0) +
-        -0.994575928740E-07 * math.pow(coldJunctionTemperature, 3.0) +
-        0.318409457190E-09  * math.pow(coldJunctionTemperature, 4.0) +
-        -0.560728448890E-12 * math.pow(coldJunctionTemperature, 5.0) +
-        0.560750590590E-15  * math.pow(coldJunctionTemperature, 6.0) +
-        -0.320207200030E-18 * math.pow(coldJunctionTemperature, 7.0) +
-        0.971511471520E-22  * math.pow(coldJunctionTemperature, 8.0) +
-        -0.121047212750E-25 * math.pow(coldJunctionTemperature, 9.0) +
-        0.118597600000E+00  * math.exp(-0.118343200000E-03 * math.pow((coldJunctionTemperature-0.126968600000E+03), 2.0)))
-
-    voltageSum = thermocoupleVoltage + coldJunctionVoltage
-    # calculate corrected temperature reading based on coefficients for 3 different ranges
-    # float b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10;
-    if thermocoupleVoltage < 0:
-        b0 = 0.0000000E+00
-        b1 = 2.5173462E+01
-        b2 = -1.1662878E+00
-        b3 = -1.0833638E+00
-        b4 = -8.9773540E-01
-        b5 = -3.7342377E-01
-        b6 = -8.6632643E-02
-        b7 = -1.0450598E-02
-        b8 = -5.1920577E-04
-        b9 = 0.0000000E+00
-    elif thermocoupleVoltage < 20.644:
-        b0 = 0.000000E+00
-        b1 = 2.508355E+01
-        b2 = 7.860106E-02
-        b3 = -2.503131E-01
-        b4 = 8.315270E-02
-        b5 = -1.228034E-02
-        b6 = 9.804036E-04
-        b7 = -4.413030E-05
-        b8 = 1.057734E-06
-        b9 = -1.052755E-08
-    elif thermocoupleVoltage < 54.886:
-        b0 = -1.318058E+02
-        b1 = 4.830222E+01
-        b2 = -1.646031E+00
-        b3 = 5.464731E-02
-        b4 = -9.650715E-04
-        b5 = 8.802193E-06
-        b6 = -3.110810E-08
-        b7 = 0.000000E+00
-        b8 = 0.000000E+00
-        b9 = 0.000000E+00
-    else:
-        # TODO: handle error - out of range
-        return 0
-    return (b0 +
-        b1 * voltageSum +
-        b2 * pow(voltageSum, 2.0) +
-        b3 * pow(voltageSum, 3.0) +
-        b4 * pow(voltageSum, 4.0) +
-        b5 * pow(voltageSum, 5.0) +
-        b6 * pow(voltageSum, 6.0) +
-        b7 * pow(voltageSum, 7.0) +
-        b8 * pow(voltageSum, 8.0) +
-        b9 * pow(voltageSum, 9.0))
+        
